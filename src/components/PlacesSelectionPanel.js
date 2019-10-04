e17a850e61204cadfffce5a2b549f04a658b0e05
Marcelina Siuda <marcelina.siuda17@gmail.com>
2019-10-04 19:14:16 +0200

    adding reserveButton function

--- src/components/PlacesSelectionPanel.js
+++ src/components/PlacesSelectionPanel.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PlacesGrid from './PlacesGrid';
 import './PlacesSelectionPanel.css';
+import axios from 'axios';
 
+const URL = 'https://cinemaapp2019.herokuapp.com/api';
 
 class PlacesSelectionPanel extends React.Component {
 
@@ -22,12 +24,25 @@ class PlacesSelectionPanel extends React.Component {
         }
     }
 
+    handleReservationClick = props => {
+
+    for (let i=1; i<=this.state.selectedPlaces.length; i++){
+        //axios.put(`${URL}/repertoir/${props.id}/${props.movieId/:props.ShowTimeId`,{
+        axios.put(`${URL}/repertoir/:5d8e684b46056b51e0b92408/:5d8e684b46056b51e0b92411/:5d8e684b46056b51e0b92416`,{
+            
+            "row": this.state.selectedPlace.row[i],
+            "sit": this.state.selectedPlace.sit[i]
+        })
+        .then(r => console.log(r.status))
+        .catch(e => console.log(e));
+    }}
+
     render() {
         return (
         <div className = 'panel'>
            <div className = 'screen'>Ekran</div>
            <PlacesGrid rows={10} sits={15} currentSelected={this.state.selectedPlaces} addSelected={this.updateSelectedPlaces}/>
-           <button className="reserveButton">Zarezerwuj</button>
+           <button className="reserveButton" onClick={this.handleReservationClick}>Zarezerwuj</button>
         </div>
         );
     }
